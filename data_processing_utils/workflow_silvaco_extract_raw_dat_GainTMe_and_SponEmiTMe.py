2006df7bffc00c01ba33dcee95d160359d92aec9
Zhe Rex Li <zhe.rex.li@gmail.com>
2025-05-08 16:50:45 +1000

    update the routine

--- data_processing_utils/workflow_silvaco_extract_raw_dat_GainTMe_and_SponEmiTMe.py
+++ data_processing_utils/workflow_silvaco_extract_raw_dat_GainTMe_and_SponEmiTMe.py
@@ -12,14 +12,9 @@ from glob import glob
 # NOTE: In each data set, it contains both the "Gain" of (TM) electrons and spontaneous emission rate for TM electrons, where is former is the negative of the absorption coefficient and don't forget to take the absolute value of it!
 sim_files_all = glob("extracted_intraband_xGa_*_dEcPer_*.dat")
 # each *dat file has the following data format:
-#"Wavelength" "Gain (TM, electrons) (well 1) (1/cm)" "Spontaneous emission (TM, electrons) (well 1) (1/(s cm3 eV))"
+# xGa dEcPer "Wavelength" "Gain (TM, electrons) (well 1) (1/cm)" "Spontaneous emission (TM, electrons) (well 1) (1/(s cm3 eV))"
 
-#first, extract the sim_wavelength
-sim_file_temp = np.genfromtxt(sim_files_all[0],skip_header=1)
-sim_wavelength = sim_file_temp[:,0] # unit: [um]
-#NOTE: the src silvaco *.dat file has the wavelength in the reverse order, i.e., it starts from 1.8 um down to 1.0 um.
-sim_wavelength = np.flipud(sim_wavelength)
-
+sim_data_processed_wavelength = np.empty((0,),dtype=np.float64) # for Gain TM
 sim_data_processed_Gain = np.empty((0,),dtype=np.float64) # for Gain TM
 sim_data_processed_Spon = np.empty((0,),dtype=np.float64) # for spontaneous emission rate
 for sim_file in sim_files_all:
@@ -29,6 +24,12 @@ for sim_file in sim_files_all:
     dEcPer_temp = sim_file[sim_file.find("dEcPer_")+len("dEcPer_"):sim_file.find(".dat")]
     dEcPer_temp = float(dEcPer_temp)
     sim_data_all = np.genfromtxt(sim_file,skip_header=1)
+
+    sim_wavelength = sim_data_all[:,0] # unit: [um]
+    #NOTE: the src silvaco *.dat file has the wavelength in the reverse order, i.e., it starts from 1.8 um down to 1.0 um.
+    sim_wavelength = np.flipud(sim_wavelength)
+    sim_data_processed_wavelength = np.hstack((sim_data_processed_wavelength,xGa_temp,dEcPer_temp,sim_wavelength))
+
     # NOTE: the electron gain is different from absorption coefficients by a negative sign
     sim_data_temp = np.abs(sim_data_all[:,1])
     sim_data_temp = np.flipud(sim_data_temp)
@@ -38,15 +39,19 @@ for sim_file in sim_files_all:
     sim_data_temp = np.flipud(sim_data_temp)
     sim_data_processed_Spon = np.hstack((sim_data_processed_Spon,xGa_temp,dEcPer_temp,sim_data_temp/sim_data_temp.max()))
 #end for
-sim_data_processed_Gain.shape = (int(sim_data_processed_Gain.size/(sim_data_temp.size+2)),sim_data_temp.size+2)
-sim_data_processed_Gain = np.vstack((np.hstack((0,0,sim_wavelength)),sim_data_processed_Gain))
-sim_data_processed_Spon.shape = (int(sim_data_processed_Spon.size/(sim_data_temp.size+2)),sim_data_temp.size+2)
-sim_data_processed_Spon = np.vstack((np.hstack((0,0,sim_wavelength)),sim_data_processed_Spon))
+sim_data_processed_wavelength.shape = (-1,sim_wavelength.size+2)
+sim_data_processed_Gain.shape = (-1,sim_data_temp.size+2)
+#sim_data_processed_Gain = np.vstack((np.hstack((0,0,sim_wavelength)),sim_data_processed_Gain))
+sim_data_processed_Spon.shape = (-1,sim_data_temp.size+2)
+#sim_data_processed_Spon = np.vstack((np.hstack((0,0,sim_wavelength)),sim_data_processed_Spon))
 # save the processed simulation data
+outfile_name_wavelength="all_simulation_data_processed_Wavelength.txt"
 outfile_name_Gain="all_simulation_data_processed_GainTMe.txt"
 outfile_name_Spon="all_simulation_data_processed_SponEmiTMe.txt"
+outfile_header_wavelength="xGa dEcPer  wavelength[um]"
 outfile_header_Gain="xGa dEcPer  AbsCoeff_normalised"
 outfile_header_Spon="xGa dEcPer  PL_normalised"
+np.savetxt(outfile_name_wavelength,sim_data_processed_wavelength,fmt='%.6g',header=outfile_header_wavelength)
 np.savetxt(outfile_name_Gain,sim_data_processed_Gain,fmt='%.6g',header=outfile_header_Gain)
 np.savetxt(outfile_name_Spon,sim_data_processed_Spon,fmt='%.6g',header=outfile_header_Spon)
 
