b75e8316cdff02398372a0afad1ba9e4aa34b6fe
Zhe Rex Li <zhe.rex.li@gmail.com>
2025-05-12 11:04:38 +1000

    update the routine by adding the wavelength output

--- data_processing_utils/workflow_silvaco_extract_raw_dat_SponEmiTMe.py
+++ data_processing_utils/workflow_silvaco_extract_raw_dat_SponEmiTMe.py
@@ -10,14 +10,10 @@ from glob import glob
 # simulated data pattern:
 # "extracted_xGa_xx_dEcPer_xx.dat"
 # NOTE: In these data sets, it was the Spontaneous emission (TM, electrons) that was extracted. 
-sim_files_all = glob("extracted_intraband_SponEmiTMe_xGa_*_dEcPer_*.dat")
-
-#first, extract the sim_wavelength
-sim_file_temp = np.genfromtxt(sim_files_all[0],skip_header=1)
-sim_wavelength = sim_file_temp[:,0] # unit: [um]
-#NOTE: the src silvaco *.dat file has the wavelength in the reverse order, i.e., it starts from 1.8 um down to 1.0 um.
-sim_wavelength = np.flipud(sim_wavelength)
-
+sim_files_all = glob("extracted_xGa_*_dEcPer_*.dat")
+# each *dat file has the following data format:
+# xGa dEcPer "Wavelength" "Gain (TM, electrons) (well 1) (1/cm)" "Spontaneous emission (TM, electrons) (well 1) (1/(s cm3 eV))"
+sim_data_processed_wavelength = np.empty((0,),dtype=np.float64) # for Gain TM
 sim_data_processed = np.empty((0,),dtype=np.float64)
 for sim_file in sim_files_all:
     print(f"**Info: process data: {sim_file}")
@@ -26,16 +22,26 @@ for sim_file in sim_files_all:
     dEcPer_temp = sim_file[sim_file.find("dEcPer_")+len("dEcPer_"):sim_file.find(".dat")]
     dEcPer_temp = float(dEcPer_temp)
     sim_data_temp = np.genfromtxt(sim_file,skip_header=1)
+
+    sim_wavelength = sim_data_temp[:,0] # unit: [um]
+    #NOTE: the src silvaco *.dat file has the wavelength in the reverse order, i.e., it starts from 1.8 um down to 1.0 um.
+    sim_wavelength = np.flipud(sim_wavelength)
+    sim_data_processed_wavelength = np.hstack((sim_data_processed_wavelength,xGa_temp,dEcPer_temp,sim_wavelength))
+
     # NOTE: even though the simualted spontaneous emission rate should be all greater than zero, it is still necessary to perform abs() 
     sim_data_temp = np.abs(sim_data_temp[:,1])
     sim_data_temp = np.flipud(sim_data_temp)
     sim_data_processed = np.hstack((sim_data_processed,xGa_temp,dEcPer_temp,sim_data_temp/sim_data_temp.max()))
 #end for
-sim_data_processed.shape = (int(sim_data_processed.size/(sim_data_temp.size+2)),sim_data_temp.size+2)
-sim_data_processed = np.vstack((np.hstack((0,0,sim_wavelength)),sim_data_processed))
+sim_data_processed_wavelength.shape = (-1,sim_wavelength.size+2)
+sim_data_processed.shape = (-1,sim_data_temp.size+2)
+
 # save the processed simulation data
+outfile_name_wavelength="all_simulation_data_processed_Wavelength.txt"
 outfile_name="all_simulation_data_processed_SponEmiTMe.txt"
+outfile_header_wavelength="xGa dEcPer  wavelength[um]"
 outfile_header="xGa dEcPer  PL_normalised"
+np.savetxt(outfile_name_wavelength,sim_data_processed_wavelength,fmt='%.6g',header=outfile_header_wavelength)
 np.savetxt(outfile_name,sim_data_processed,fmt='%.6g',header=outfile_header)
 
 # test plot
