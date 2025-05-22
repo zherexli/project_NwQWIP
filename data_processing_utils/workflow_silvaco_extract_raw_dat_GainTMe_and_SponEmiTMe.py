b73802f66da3fd2e4a4c51456ad5bb7551daff45
Zhe Rex Li <zhe.rex.li@gmail.com>
2025-05-22 14:18:48 +1000

    update the routine by adding the feature to extract the peak TM Gain values

--- data_processing_utils/workflow_silvaco_extract_raw_dat_GainTMe_and_SponEmiTMe.py
+++ data_processing_utils/workflow_silvaco_extract_raw_dat_GainTMe_and_SponEmiTMe.py
@@ -14,9 +14,10 @@ sim_files_all = glob("extracted_intraband_xGa_*_dEcPer_*.dat")
 # each *dat file has the following data format:
 # xGa dEcPer "Wavelength" "Gain (TM, electrons) (well 1) (1/cm)" "Spontaneous emission (TM, electrons) (well 1) (1/(s cm3 eV))"
 
-sim_data_processed_wavelength = np.empty((0,),dtype=np.float64) # for Gain TM
-sim_data_processed_Gain = np.empty((0,),dtype=np.float64) # for Gain TM
-sim_data_processed_Spon = np.empty((0,),dtype=np.float64) # for spontaneous emission rate
+sim_data_processed_wavelength = np.empty((0,),dtype=np.float64) # for wavelength
+sim_data_processed_Gain       = np.empty((0,),dtype=np.float64) # for Gain TM
+sim_data_processed_Spon       = np.empty((0,),dtype=np.float64) # for spontaneous emission rate
+sim_data_processed_GainPeak   = np.empty((0,),dtype=np.float64) # for peak Gain TM
 for sim_file in sim_files_all:
     print(f"**Info: process data: {sim_file}")
     xGa_temp = sim_file[sim_file.find("xGa_")+len("xGa_"):sim_file.find("_dEcPer")]
@@ -34,6 +35,10 @@ for sim_file in sim_files_all:
     sim_data_temp = np.abs(sim_data_all[:,1])
     sim_data_temp = np.flipud(sim_data_temp)
     sim_data_processed_Gain = np.hstack((sim_data_processed_Gain,xGa_temp,dEcPer_temp,sim_data_temp/sim_data_temp.max()))
+    # also extract the peak Gain value and its corresponding wavelength
+    peak_gain_idx=np.argmax(sim_data_temp)
+    sim_data_processed_GainPeak = np.hstack((sim_data_processed_GainPeak,xGa_temp,dEcPer_temp,\
+                                             sim_wavelength[peak_gain_idx],sim_data_temp[peak_gain_idx]))
 
     sim_data_temp = np.abs(sim_data_all[:,2])
     sim_data_temp = np.flipud(sim_data_temp)
@@ -41,6 +46,7 @@ for sim_file in sim_files_all:
 #end for
 sim_data_processed_wavelength.shape = (-1,sim_wavelength.size+2)
 sim_data_processed_Gain.shape = (-1,sim_data_temp.size+2)
+sim_data_processed_GainPeak.shape = (-1,4)
 #sim_data_processed_Gain = np.vstack((np.hstack((0,0,sim_wavelength)),sim_data_processed_Gain))
 sim_data_processed_Spon.shape = (-1,sim_data_temp.size+2)
 #sim_data_processed_Spon = np.vstack((np.hstack((0,0,sim_wavelength)),sim_data_processed_Spon))
@@ -48,12 +54,15 @@ sim_data_processed_Spon.shape = (-1,sim_data_temp.size+2)
 outfile_name_wavelength="all_simulation_data_processed_Wavelength.txt"
 outfile_name_Gain="all_simulation_data_processed_GainTMe.txt"
 outfile_name_Spon="all_simulation_data_processed_SponEmiTMe.txt"
+outfile_name_GainPeak="all_simulation_data_processed_PeakGainTMe.txt"
 outfile_header_wavelength="xGa dEcPer  wavelength[um]"
 outfile_header_Gain="xGa dEcPer  AbsCoeff_normalised"
 outfile_header_Spon="xGa dEcPer  PL_normalised"
+outfile_header_GainPeak="xGa dEcPer  Wavelength_at_peak_gain[um] peak_TMGain[1/cm]"
 np.savetxt(outfile_name_wavelength,sim_data_processed_wavelength,fmt='%.6g',header=outfile_header_wavelength)
 np.savetxt(outfile_name_Gain,sim_data_processed_Gain,fmt='%.6g',header=outfile_header_Gain)
 np.savetxt(outfile_name_Spon,sim_data_processed_Spon,fmt='%.6g',header=outfile_header_Spon)
+np.savetxt(outfile_name_GainPeak,sim_data_processed_GainPeak,fmt='%.6g',header=outfile_header_GainPeak)
 
 # test plot
 #plt.plot(sim_data_processed[0,2:],sim_data_processed[1,2:])
